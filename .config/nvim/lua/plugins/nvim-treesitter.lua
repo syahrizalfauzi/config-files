f4c3a5a139575948889c95c29f48dcda2124e1dd
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2025-03-12 14:06:25 +0700

    nvim : add tresitter for laravel blade

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -8,6 +8,23 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
   },
   config = function()
+    -- configure for tree-sitter-blade for Laravel Blade syntax
+    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade"
+    }
+
+    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+      pattern = "*.blade.php",
+      command = "set filetype=blade",
+      group = vim.api.nvim_create_augroup("BladeFiletypeRelated", { clear = true }),
+    })
+
     -- import nvim-treesitter plugin
     local treesitter = require "nvim-treesitter.configs"
 

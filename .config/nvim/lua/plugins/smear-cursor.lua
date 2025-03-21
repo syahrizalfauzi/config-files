089ce6acc1214258952879ea4554d81e75f19455
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2025-03-22 01:29:38 +0700

    nvim : add smear-cursor

---
+++ .config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  lazy = false,
+  opts = {                         -- Default  Range
+    distance_stop_animating = 1, -- 0.1      > 0
+    smear_terminal_mode = true,
+    time_interval = 8,
+    max_length = 100
+  },
+}

fd7197d2490584b3a283a4b4127b0f0a02bf90fd
M. Syahrizal Fauzi <m.syahrizal@corp.bri.co.id>
2025-03-30 13:45:01 +0700

    nvim : disable blink ghost text, change colorscheme to catpuccin

---
+++ .config/nvim/lua/plugins/catpuccin.lua
@@ -0,0 +1,24 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+    integrations = {
+      blink_cmp = true,
+      cmp = true,
+      gitsigns = true,
+      harpoon = true,
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      nvimtree = true,
+      telescope = true,
+      treesitter = true,
+      which_key = true,
+    },
+  },
+}
